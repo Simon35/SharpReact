7db5445a94aa7ee388d02dd7f6526fde2d8c2230
simon <simongouzien@hotmail.fr>
2023-05-22 12:23:37 +0200

    Ajoutez des fichiers projet.

---
+++ SharpReact/Model/Citizen.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpReact.Model
+{
+    public class Citizen
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ SharpReact/Model/CitizenContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SharpReact.Model
+{
+    public class CitizenContext:DbContext
+    {
+        public CitizenContext(DbContextOptions<CitizenContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Citizen> Citizens { get; set; }
+    }
+
+}
