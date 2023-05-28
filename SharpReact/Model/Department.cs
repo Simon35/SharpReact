222276a3784365f0fc858da1fe9ac9c9151e2384
simon <simongouzien@hotmail.fr>
2023-05-28 14:14:58 +0200

    add Models employee and department

---
+++ SharpReact/Model/Department.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SharpReact.Model
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ SharpReact/Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SharpReact.Model
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
